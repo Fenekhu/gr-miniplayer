e68b31a0dfa2a591d178f2d7a8f196e5ac2110b2
Fenekhu <firebirdspiral@pm.me>
2025-03-14 17:35:04 -0500

    adjust icon sizes

--- lib/util/lib/app_style.dart
+++ lib/util/lib/app_style.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const double controlIconBoxSize = 32;
-const double controlIconSize = controlIconBoxSize - 0;
-const double menuIconBoxSize = 24;
-const double menuIconSize = menuIconBoxSize - 0;
+const double controlIconSize = controlIconBoxSize - 4;
+const double menuIconBoxSize = 20;
+const double menuIconSize = menuIconBoxSize - 4;
 const double windowIconBoxSize = 24;
 const double windowIconSize = windowIconBoxSize - 8;
 
