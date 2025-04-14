ad6b9d11dc62361e637910996d7936ca85f5ac62
Fenekhu <firebirdspiral@pm.me>
2025-04-14 16:48:40 -0500

    automatically update version

--- lib/util/lib/app_info.dart
+++ lib/util/lib/app_info.dart
@@ -4,5 +4,5 @@ library;
 import 'dart:io';
 
 const String name = 'Gensokyo Radio Miniplayer';
-const String version = '0.1.0';
+const String version = String.fromEnvironment('FLUTTER_BUILD_NAME', defaultValue: 'unknownver');
 final String userAgent = 'GR Miniplayer/$version (${Platform.operatingSystem}; github.com/Fenekhu/gr_miniplayer)';
